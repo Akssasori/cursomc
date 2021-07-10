f7f254cb8840d4cf270be608d3aebd7cfbf7b937
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-07-10 11:35:16 -0300

    testando o get

--- src/main/java/com/lucas/resources/CategoriaResource.java
+++ src/main/java/com/lucas/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package com.lucas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.lucas.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest está funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Escritorio");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		return lista;
 	}
 
 }
