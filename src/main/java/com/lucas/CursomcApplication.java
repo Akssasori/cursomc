73b58b1f1cee103dec3afc36f88c1df4c2465830
Lucas Diniz <53830752+Akssasori@users.noreply.github.com>
2021-07-12 22:01:14 -0300

    relacionamento produtos categoria

--- src/main/java/com/lucas/CursomcApplication.java
+++ src/main/java/com/lucas/CursomcApplication.java
@@ -1,13 +1,38 @@
 package com.lucas;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.lucas.domain.Categoria;
+import com.lucas.domain.Produto;
+import com.lucas.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null,"Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00, null);
+		Produto p2 = new Produto(null, "Impressora", 800.00, null);
+		Produto p3 = new Produto(null, "Mouse", 80.00, null);
+		
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
